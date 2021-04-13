c2d78b58928706e42298eb2e7f61b27d7fba760e
Roman maluf <eliasmaluf51@gmail.com>
2021-04-12 22:38:12 -0300

    Chagen application name

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,8 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-module MessageMe
+# Change Me to the Application Name
+module MyOwnRubyTemplate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
